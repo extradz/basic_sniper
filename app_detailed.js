5b68f26966cb3da9c6bd2298aaccfc1570165de7
extradz <114875215+extradz@users.noreply.github.com>
2023-11-07 23:44:11 +0100

    Create app_detailed.js
    add block number and position at the end

---
+++ app_detailed.js
@@ -0,0 +1,200 @@
+"use strict";
+const env = require("./env.json");
+Object.assign(process.env, env);
+
+const ethers = require("ethers");
+const retry = require("async-retry");
+const tokens = require("./tokens.js");
+const pcsAbi = new ethers.utils.Interface(require("./abi.json"));
+const tokenAbi = new ethers.utils.Interface(require("./tokenABI.json"));
+const EXPECTED_PONG_BACK = 30000;
+const KEEP_ALIVE_CHECK_INTERVAL = 15000;
+let pingTimeout = null;
+let keepAliveInterval = null;
+let provider;
+let wallet;
+let account;
+let router;
+let grasshopper;
+let swapEth;
+let purchaseAmount;
+
+async function Wait(seconds) {
+  return new Promise((resolve) => {
+    setTimeout(resolve, seconds * 1000);
+  });
+}
+
+const startConnection = () => {
+  provider = new ethers.providers.WebSocketProvider(process.env.BSC_NODE_WSS);
+  wallet = new ethers.Wallet(process.env.PRIVATE_KEY);
+  account = wallet.connect(provider);
+  router = new ethers.Contract(tokens.router, pcsAbi, account);
+
+  grasshopper = 0;
+  provider._websocket.on("open", async () => {
+    console.log("waiting for liquidity...");
+    keepAliveInterval = setInterval(() => {
+      provider._websocket.ping();
+      pingTimeout = setTimeout(() => {
+        provider._websocket.terminate();
+      }, EXPECTED_PONG_BACK);
+    }, KEEP_ALIVE_CHECK_INTERVAL);
+    const WETH = await router.WETH();
+    if (ethers.utils.getAddress(tokens.pair[0]) === ethers.utils.getAddress(WETH)) {
+      swapEth = 1;
+      purchaseAmount = ethers.utils.parseUnits(tokens.purchaseAmount, "ether");
+    } else {
+      await Approve();
+    }
+  });
+
+  provider.on("pending", async (txHash) => {
+    provider
+      .getTransaction(txHash)
+      .then(async (tx) => {
+        if (grasshopper === 0) {
+          console.log("waiting for liquidity ...");
+          grasshopper = 1;
+        }
+        if (tx && tx.to) {
+          if (ethers.utils.getAddress(tx.to) ===
+              ethers.utils.getAddress(tokens.router)){
+            const re1 = new RegExp("^0xf305d719");
+            const re2 = new RegExp("^0xe8e33700");
+            if (re1.test(tx.data) || re2.test(tx.data)) {
+              const decodedInput = pcsAbi.parseTransaction({
+                data: tx.data,
+                value: tx.value,
+              });
+              if (
+                ethers.utils.getAddress(tokens.pair[1]) ===
+                ethers.utils.getAddress(decodedInput.args[0]) ||
+                ethers.utils.getAddress(tokens.pair[1]) ===
+                ethers.utils.getAddress(decodedInput.args[1])
+              ) {
+                provider.off("pending");
+                if (tokens.buyDelay > 0) {
+                  await Wait(tokens.buyDelay);
+                }
+                console.log(new Date().toISOString() + " - Liquidity spotted at:");
+                await BuyToken(tx);
+              }
+            }
+          }
+        }
+      })
+      .catch(() => {});
+  });
+
+  provider._websocket.on("close", () => {
+    console.log("WebSocket Closed...Reconnecting...");
+    clearInterval(keepAliveInterval);
+    clearTimeout(pingTimeout);
+    startConnection();
+  });
+
+  provider._websocket.on("error", () => {
+    console.log("Error. Attempting to Reconnect...");
+    clearInterval(keepAliveInterval);
+    clearTimeout(pingTimeout);
+    startConnection();
+  });
+
+  provider._websocket.on("pong", () => {
+    clearInterval(pingTimeout);
+  });
+};
+
+const Approve = async () => {
+  const contract = new ethers.Contract(
+    tokens.pair[0],
+    tokenAbi,
+    account
+  );
+  const tokenName = await contract.name();
+  const tokenDecimals = await contract.decimals();
+  purchaseAmount = ethers.utils.parseUnits(tokens.purchaseAmount, tokenDecimals);
+  const allowance = await contract.allowance(process.env.RECIPIENT, tokens.router);
+  if (allowance._hex === "0x00") {
+    const tx = await contract.approve(tokens.router, ethers.constants.MaxUint256);
+    const receipt = await tx.wait();
+    console.log(`Approved ${tokenName} for swapping... ${receipt.transactionHash}`);
+  }
+};
+
+const BuyToken = async (txLP) => {
+  console.log(new Date().toISOString() + " - Sending buy transaction at:");
+  const tx = await retry(
+    async () => {
+      const amountOutMin = 0;
+      if (swapEth) {
+        const reciept = await router.swapExactETHForTokens(
+          amountOutMin,
+          tokens.pair,
+          process.env.RECIPIENT,
+          Date.now() + 1000 * tokens.deadline,
+          {
+            value: purchaseAmount,
+            gasLimit: tokens.gasLimit,
+            gasPrice: txLP.gasPrice,
+          }
+        );
+        return reciept;
+      } else {
+        const reciept = await router.swapExactTokensForTokens(
+          purchaseAmount,
+          amountOutMin,
+          tokens.pair,
+          process.env.RECIPIENT,
+          Date.now() + 1000 * tokens.deadline,
+          {
+            gasLimit: tokens.gasLimit,
+            gasPrice: txLP.gasPrice,
+          }
+        );
+        return reciept;
+      }
+    },
+    {
+      retries: tokens.buyRetries,
+      minTimeout: tokens.retryMinTimeout,
+      maxTimeout: tokens.retryMaxTimeout,
+      onRetry: (err, number) => {
+        console.log("Buy Failed - Retrying", number);
+        console.log(err);
+        if (number === tokens.buyRetries) {
+          console.log("Sniping has failed...");
+          process.exit();
+        }
+      },
+    }
+  );
+  console.log("LP: " + txLP.hash);
+  console.log("Sniped: " + tx.hash);
+
+  // After the buy transaction, we wait for 5 seconds to ensure the transactions have likely been mined
+  await Wait(5);
+
+  // Fetch the transaction receipt for the LP transaction
+  const receiptLP = await provider.getTransactionReceipt(txLP.hash);
+  if (receiptLP) {
+    console.log(`LP Transaction Block Number: ${receiptLP.blockNumber}`);
+    console.log(`LP Transaction Position in Block: ${receiptLP.transactionIndex}`);
+  } else {
+    console.log("LP Transaction Receipt not found after 5 seconds.");
+  }
+
+  // Fetch the transaction receipt for the Buy transaction
+  const receiptBuy = await provider.getTransactionReceipt(tx.hash);
+  if (receiptBuy) {
+    console.log(`Buy Transaction Block Number: ${receiptBuy.blockNumber}`);
+    console.log(`Buy Transaction Position in Block: ${receiptBuy.transactionIndex}`);
+  } else {
+    console.log("Buy Transaction Receipt not found after 5 seconds.");
+  }
+
+  process.exit();
+};
+
+startConnection();
